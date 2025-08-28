46e6c3d52b196c1d75b4a42c7d479ccd08566f54
NikRaines <Nikkelas@raines.net.au>
2025-08-29 08:26:15 +1000

    inital generates

---
+++ src/app/components/channel-chat/channel-chat.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-channel-chat',
+  imports: [],
+  templateUrl: './channel-chat.html',
+  styleUrl: './channel-chat.scss'
+})
+export class ChannelChat {
+
+}
